ba53875eeceb11e56f6708e128590cd18008571e
Thomas Heller <thomas_heller@hotmail.com>
2026-05-28 01:46:22 -0400

    Temporary disable notion

--- lib/lib/notion.ts
+++ lib/lib/notion.ts
@@ -1,18 +1,3 @@
-import { Client } from '@notionhq/client';
-
-const notion = new Client({
-  auth: process.env.NOTION_API_KEY,
-});
-
 export async function getPropiedades() {
-  try {
-    const response = await notion.databases.query({
-      database_id: process.env.NOTION_DATABASE_ID!,
-    });
-
-    return response.results;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+  return [];
 }
